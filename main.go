32e0db17e3a7b0d7c0e64654c33ce2edd8199744
Darron Froese <darron@froese.org>
2016-03-10 22:26:32 -0500

    Added `-v` and `--version` command.
    This helps to figure out what version you're using.

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/darron/sifter/commands"
 	"log"
 	"log/syslog"
+	"os"
 	"runtime"
+)
+
+var (
+	// CompileDate tracks when the binary was compiled. It's inserted during a build
+	// with build flags. Take a look at the Makefile for information.
+	CompileDate = "No date provided."
+
+	// GitCommit tracks the SHA of the built binary. It's inserted during a build
+	// with build flags. Take a look at the Makefile for information.
+	GitCommit = "No revision provided."
+
+	// Version is the version of the built binary. It's inserted during a build
+	// with build flags. Take a look at the Makefile for information.
+	Version = "No version provided."
+
+	// GoVersion details the version of Go this was compiled with.
+	GoVersion = runtime.Version()
 )
 
 func main() {
@@ -12,6 +31,14 @@ func main() {
 	if e == nil {
 		log.SetOutput(logwriter)
 	}
-	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "-v" || arg == "--version" {
+			fmt.Printf("%-8s : %s\n%-8s : %s\n%-8s : %s\n%-8s : %s\n", "Version", Version, "Revision", GitCommit, "Date", CompileDate, "Go", GoVersion)
+			os.Exit(0)
+		}
+	}
+
 	commands.RootCmd.Execute()
 }
